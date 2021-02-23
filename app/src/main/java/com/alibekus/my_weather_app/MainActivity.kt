99e0849bea1a9084f8541cb2a0d4a5428852af26
alibek.nurgaliyev <alibek.nurgaliyev@gmail.com>
2021-02-23 19:47:08 +0600

    retrofit and Gson parsing implemented

--- app/src/main/java/com/alibekus/my_weather_app/MainActivity.kt
+++ app/src/main/java/com/alibekus/my_weather_app/MainActivity.kt
@@ -2,10 +2,78 @@ package com.alibekus.my_weather_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private var weatherData: TextView? = null
+
+private lateinit var getDataButton: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        weatherData = findViewById(R.id.textView)
+        getDataButton = findViewById(R.id.getButton)
+        getDataButton.setOnClickListener {
+            getCurrentWeatherData()
+        }
+    }
+
+    internal fun getCurrentWeatherData() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(WeatherService::class.java)
+        val call = service.getCurrentWeatherData(lat, lon, AppId)
+
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(
+                call: Call<WeatherResponse>,
+                response: Response<WeatherResponse>
+            ) {
+                if (response.code() == 200) {
+                    val weatherResponse = response.body()!!
+                    val stringBuilder = "Country: " +
+                            weatherResponse.sys!!.country +
+                            "\n" +
+                            "Temperature: " +
+                            weatherResponse.main!!.temp +
+                            "\n" +
+                            "Temperature(Min): " +
+                            weatherResponse.main!!.temp_min +
+                            "\n" +
+                            "Temperature(Max): " +
+                            weatherResponse.main!!.temp_max +
+                            "\n" +
+                            "Humidity: " +
+                            weatherResponse.main!!.humidity +
+                            "\n" +
+                            "Pressure: " +
+                            weatherResponse.main!!.pressure
+
+                    weatherData!!.text = stringBuilder
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                weatherData!!.text = "No internet"
+            }
+        })
+    }
+
+
+    companion object {
+        var BaseUrl = "http://api.openweathermap.org/"
+        var AppId = "2e65127e909e178d0af311a81f39948c"
+        var lat = "35"
+        var lon = "139"
     }
 }
